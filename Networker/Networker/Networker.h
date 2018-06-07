ca299b89fd04b15daead2f6bc461bf2e73266ee2
Egemen Ayhan <egemen.ayhan@turkcell.com.tr>
2018-06-07 18:15:45 +0300

    Project structure created, Carthage integration added.
    Alamofire added.

---
+++ Networker/Networker/Networker.h
@@ -0,0 +1,19 @@
+//
+//  Networker.h
+//  Networker
+//
+//  Created by EGEMEN AYHAN on 7.06.2018.
+//  Copyright © 2018 NSIstanbul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Networker.
+FOUNDATION_EXPORT double NetworkerVersionNumber;
+
+//! Project version string for Networker.
+FOUNDATION_EXPORT const unsigned char NetworkerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Networker/PublicHeader.h>
+
+
