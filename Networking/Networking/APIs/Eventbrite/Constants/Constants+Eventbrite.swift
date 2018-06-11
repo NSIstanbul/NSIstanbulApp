b6556924cddc049b2b3f5159c6c911a3ffdce31d
Said Ozcan <said.ozcan@booking.com>
2018-06-11 03:03:47 +0200

    Add Constants
    This commit adds Constants and EventbriteConstants

---
+++ Networking/Networking/APIs/Eventbrite/Constants/Constants+Eventbrite.swift
@@ -0,0 +1,14 @@
+//
+//  Constants+Eventbrite.swift
+//  Networking
+//
+//  Created by Said Ozcan on 11/06/2018.
+//  Copyright © 2018 NSIstanbul. All rights reserved.
+//
+
+import Foundation
+
+extension Constants.Eventbrite {
+    static let organizationID: String = "13871494483"
+    static let credentialsFileName: String = "EventbriteCredentials"
+}
