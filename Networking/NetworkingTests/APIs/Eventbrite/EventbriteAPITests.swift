0933cefcc309cde39b443af4c665b4451798d420
Said Ozcan <said.ozcan@booking.com>
2018-06-14 03:58:00 +0200

    fix copyrights

--- Networking/NetworkingTests/APIs/Eventbrite/EventbriteAPITests.swift
+++ Networking/NetworkingTests/APIs/Eventbrite/EventbriteAPITests.swift
@@ -3,7 +3,7 @@
 //  NetworkingTests
 //
 //  Created by Said Ozcan on 14/06/2018.
-//  Copyright © 2018 Ilter Cengiz. All rights reserved.
+//  Copyright © 2018 NSIstanbul. All rights reserved.
 //
 
 import XCTest
