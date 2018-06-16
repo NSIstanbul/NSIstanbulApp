561f845894fdf2af8ecafae5382c8f366cd5f262
Said Ozcan <said.ozcan@booking.com>
2018-06-17 01:50:48 +0200

    rename EventsList.json to EventListResponse.json

--- Networking/NetworkingTests/APIs/Eventbrite/EventbriteAPITests.swift
+++ Networking/NetworkingTests/APIs/Eventbrite/EventbriteAPITests.swift
@@ -13,7 +13,7 @@ class EventbriteAPITests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        Stubber.stubRequest(EventbriteEventsRequest(), withJSONFromFile: "EventsList.json")
+        Stubber.stubRequest(EventbriteEventsRequest(), withJSONFromFile: "EventsListResponse.json")
     }
     
     func testEventsList() {
@@ -45,6 +45,7 @@ class EventbriteAPITests: XCTestCase {
             
             XCTAssertEqual(firstEvent.venue.name, "sahibinden.com")
             XCTAssertTrue(firstEvent.availability.isSoldOut)
+            XCTAssertEqual(firstEvent.logo.url?.absoluteString, "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F45404553%2F21793522196%2F1%2Foriginal.jpg?h=200&w=450&auto=compress&rect=0%2C0%2C2160%2C1080&s=1c506d226e5329f74e90497e48f3bce9")
         case .failure(let error):
             XCTFail("Request failed: \(error)")
         }
