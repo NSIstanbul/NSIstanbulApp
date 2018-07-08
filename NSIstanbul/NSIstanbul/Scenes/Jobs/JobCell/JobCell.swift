adc6fac945316f93a866931c1caba7973a34c864
Said Ozcan <said.ozcan@booking.com>
2018-07-08 13:12:19 +0200

    add initial Jobs module files

---
+++ NSIstanbul/NSIstanbul/Scenes/Jobs/JobCell/JobCell.swift
@@ -0,0 +1,24 @@
+//
+//  JobCell.swift
+//  NSIstanbul
+//
+//  Created by Said Ozcan on 07/07/2018.
+//  Copyright © 2018 NSIstanbul. All rights reserved.
+//
+
+import UIKit
+
+class JobCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ NSIstanbul/NSIstanbul/Scenes/Jobs/JobsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  JobsViewModel.swift
+//  NSIstanbul
+//
+//  Created by Said Ozcan on 07/07/2018.
+//  Copyright © 2018 NSIstanbul. All rights reserved.
+//
+
+import Foundation
